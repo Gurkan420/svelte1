f268f1e3e2fc9badecbccc93d9653269d6a2c503
August Ljungdell <august.ljungdell@skola.botkyrka.se>
2021-05-30 22:04:25 +0200

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,56 @@
+<script>
+	
+</script>
+
+<main>
+	<div style="background-image: url('släng1.jpg');"></div>
+		
+<style>
+	div {
+  background-image: url('släng1.jpg');
+}
+	main {
+		background-image: url("släng1.jpg")
+		
+		text-align: center;
+		padding: 2em;
+	
+	}
+
+	h1 {
+		color: #504643;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 500;
+		height: 5000px
+	}
+
+
+		}
+</style>
+	
+		<div class="menybar">
+		  <a href="#Wiki">Wiki</a>
+
+		  <div class="submeny">
+			<button class="submenybtn">Forum</button>
+			<div class="submeny-content">
+			</div>
+		    </div>
+		    <div class="submeny">
+			<button class="submenybtn">Information</button>
+		   	 <div class="submeny-content">
+			</div>
+			 </div>
+		      <div class="submeny">
+			  <button class="submenybtn">Ansök</button>
+			  <div class="submeny-content">
+		    </div>
+		    </div>
+
+	<h1>Social media cloud</h1>
+	
+	
+
+</main>
+
